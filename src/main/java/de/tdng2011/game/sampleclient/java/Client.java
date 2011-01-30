f6a5cbe648d330ccae697ac809e8c37531f2419b
Benjamin Neff <benjamin@coding4coffee.ch>
2011-01-30 20:01:23 +0100

    prepare java sample client

---
+++ src/main/java/de/tdng2011/game/sampleclient/java/Client.java
@@ -0,0 +1,14 @@
+package de.tdng2011.game.sampleclient.java;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: benjamin
+ * Date: 30.01.11
+ * Time: 19:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class Client {
+    public static void main(String... args) {
+        System.out.println("Sample java client");
+    }
+}
