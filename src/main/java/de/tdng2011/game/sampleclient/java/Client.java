4dc983eddc0dc458f3eaf3fcae5e90bdd54e05bc
Benjamin Neff <benjamin@coding4coffee.ch>
2011-03-11 03:04:29 +0100

    cleanup

--- src/main/java/de/tdng2011/game/sampleclient/java/Client.java
+++ src/main/java/de/tdng2011/game/sampleclient/java/Client.java
@@ -1,41 +1,34 @@
 package de.tdng2011.game.sampleclient.java;
 
+import java.io.IOException;
+
 import de.tdng2011.game.library.World;
-import de.tdng2011.game.library.ScoreBoard;
 import de.tdng2011.game.library.connection.AbstractClient;
 import de.tdng2011.game.library.connection.RelationTypes;
-import scala.collection.JavaConversions;
-import scala.collection.immutable.List;
-import scala.collection.mutable.Buffer;
-
-import java.io.IOException;
-import java.util.Arrays;
 
 public class Client extends AbstractClient {
 
-    private static Client client;
+	private static Client client;
 
-    public Client(final String hostname) {
-        super(hostname, RelationTypes.Player(), true);
-    }
+	public Client(final String hostname) {
+		super(hostname, RelationTypes.Player(), true);
+	}
 
-    public static void main(String... args) throws IOException {
-        System.out.println("Sample java client");
+	public static void main(final String... args) throws IOException {
+		System.out.println("Sample java client");
 
-        client = new Client("test.scubywars.de");
+		client = new Client("test.scubywars.de");
 
-        client.action(true, false, true, true);
-    }
+		client.action(true, false, true, true);
+	}
 
-    public String name() {
-        return "Java-Client";
-    }
+	@Override
+	public String name() {
+		return "Java-Client";
+	}
 
-    public void processWorld(World world) {
-        // TODO
-    }
-
-    //public static Buffer asScalaBuffer(Object... objects) {
-    //    return JavaConversions.asScalaBuffer(Arrays.asList(objects));
-    //}
+	@Override
+	public void processWorld(final World world) {
+		// TODO
+	}
 }
