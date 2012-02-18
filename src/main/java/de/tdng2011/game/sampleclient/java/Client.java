3ec1f5e65e58ced2cbf4fc321b56b8acdd1d9ce4
Benjamin Neff <benjamin@coding4coffee.ch>
2012-02-18 20:42:09 +0100

    throws exception

--- src/main/java/de/tdng2011/game/sampleclient/java/Client.java
+++ src/main/java/de/tdng2011/game/sampleclient/java/Client.java
@@ -3,12 +3,12 @@ package de.tdng2011.game.sampleclient.java;
 import java.io.IOException;
 import java.util.List;
 
+import scala.collection.JavaConversions;
 import de.tdng2011.game.library.Player;
 import de.tdng2011.game.library.Shot;
 import de.tdng2011.game.library.World;
 import de.tdng2011.game.library.connection.AbstractClient;
 import de.tdng2011.game.library.connection.RelationTypes;
-import scala.collection.JavaConversions;
 
 public class Client extends AbstractClient {
 
@@ -32,9 +32,10 @@ public class Client extends AbstractClient {
 	}
 
 	@Override
-	public void processWorld(final World world) {
-		List<Player> players = JavaConversions.asJavaList(world.players());
-		List<Shot> shots = JavaConversions.asJavaList(world.shots());
+	public void processWorld(final World world) throws Exception {
+		final List<Player> players = JavaConversions
+				.asJavaList(world.players());
+		final List<Shot> shots = JavaConversions.asJavaList(world.shots());
 		// TODO
 	}
 }
